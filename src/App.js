17be67c8e1f3b1c3bc043e4dabd13a813e7129fe
mukkasaiteja <msaiteja2669@gmail.com>
2023-04-21 17:49:08 +0530

    jsx assignment-5

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='navbar'>
+
+      <section className='logo'>lOGO</section>
+      <section className='nav-iteams'>
+       <ul>
+        <li><span>HOME</span></li>
+        <li>ABOUT</li>
+        <li>MENU</li>
+        <li>ORDER</li>
+        <li>CONTACT</li>
+
+       </ul>
+      </section>
+      </div>
+      <div className='div'><h1>HOME</h1></div>
+
+      
+    </div>
+  );
+}
+
+export default App;
